173a20a313390c6932987efc3fccd17b81f67449
EvgeniyMukhamedjanov <evgeniymuhamedjanov@gmail.com>
2021-10-16 22:57:30 +0500

    Update docs.js

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -26,24 +26,4 @@ $(function() {
             $btn.text('Hide');
         }
     });
-
-    // Make the triangular pattern in the header
-    if (uiColors) {
-        const $masthead = $('.site-masthead');
-
-        if ($masthead.length) {
-            const t = new Trianglify({
-                cellsize: 90,
-                noiseIntensity: 0,
-                x_gradient: [
-                    uiColors[0],
-                    uiColors[1],
-                ],
-            });
-            const pattern = t.generate(window.screen.width | $masthead.outerWidth(), $masthead.outerHeight() * 1.2);
-
-            const style = $('<style>.site-masthead { background-image: ' + pattern.dataUrl + '; }</style>');
-            $('html > head').append(style);
-        }
-    }
 });
