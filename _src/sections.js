51b9843939763dde90c81efac893f646547ea4b2
EvgeniyMukhamedjanov <evgeniymuhamedjanov@gmail.com>
2021-10-30 16:17:58 +0500

    Add functionality to update only part of a data-ajax-cart-section sections

--- _src/sections.js
+++ _src/sections.js
@@ -11,9 +11,16 @@ const cartSectionsInit = () => {
 			const sectionNames = [];
 			// todo: test with dynamic sections
 			document.querySelectorAll( `[${ sectionsAttribute }]` ).forEach( sectionNodeChild => {
-				let sectionIdHTML = sectionNodeChild.parentElement.id;
-				if ( sectionIdHTML.indexOf( shopifySectionPrefix ) === 0 ) {
-					const sectionId = sectionIdHTML.replace( shopifySectionPrefix, '' );
+				const sectionNode = sectionNodeChild.closest(`[id^="${ shopifySectionPrefix }"]`);
+				// let sectionIdHTML = sectionNodeChild.parentElement.id;
+				// if ( sectionIdHTML.indexOf( shopifySectionPrefix ) === 0 ) {
+				// 	const sectionId = sectionIdHTML.replace( shopifySectionPrefix, '' );
+				// 	if ( sectionNames.indexOf( sectionId ) === -1 ) {
+				// 		sectionNames.push( sectionId );
+				// 	}
+				// }
+				if ( sectionNode ) {
+					const sectionId = sectionNode.id.replace( shopifySectionPrefix, '' );
 					if ( sectionNames.indexOf( sectionId ) === -1 ) {
 						sectionNames.push( sectionId );
 					}
@@ -31,14 +38,17 @@ const cartSectionsInit = () => {
 		subscribeToResult( data => {
 			const { sectionsAttribute, sectionScrollAreaAttribute } = settings.computed;
 
-			if ( 'responseData' in data && data.responseData.ok && 'sections' in data.responseData.body ) {
+			if ( data.responseData?.ok && 'sections' in data.responseData.body ) {
 				const sections = data.responseData.body.sections;
 				for ( let sectionId in sections ) {
-					document.querySelectorAll( `#shopify-section-${ sectionId } > [${ sectionsAttribute }]` ).forEach( sectionNodeChild => {
-						const noId = "__noId__"
+					document.querySelectorAll( `#shopify-section-${ sectionId }` ).forEach( sectionNode => {
+
+						let newSectionNode = sectionNode;
+
+						// Save scroll positions
+						const noId = "__noId__";
 						const scrollAreasList = {};
-						const oldSectionNode = sectionNodeChild.parentNode;
-						sectionNodeChild.parentNode.querySelectorAll(` [${ sectionScrollAreaAttribute }] `).forEach( scrollAreaNode => {
+						sectionNode.querySelectorAll(` [${ sectionScrollAreaAttribute }] `).forEach( scrollAreaNode => {
 							let scrollId = scrollAreaNode.getAttribute( sectionScrollAreaAttribute ).toString().trim();
 							if ( scrollId === '' ) {
 								scrollId = noId;
@@ -48,9 +58,35 @@ const cartSectionsInit = () => {
 							}
 							scrollAreasList[scrollId].push(scrollAreaNode.scrollTop);
 						});
-						oldSectionNode.insertAdjacentHTML('beforeBegin', sections[ sectionId ]);
-						const newSectionNode = oldSectionNode.previousSibling;
-						oldSectionNode.parentElement.removeChild(oldSectionNode);
+
+						// Replace HTML
+						let updateFullSection = false;
+						if ( sectionNode.querySelector(`:scope > [${ sectionsAttribute }]`) ) {
+							updateFullSection = true;
+						} else {
+							const sectionParts = sectionNode.querySelectorAll( `[${ sectionsAttribute }]` );
+							if ( sectionParts ) {
+								const parser = new DOMParser();
+          						const receivedDOM = parser.parseFromString(sections[ sectionId ], "text/html");
+          						const receivedParts = receivedDOM.querySelectorAll( `[${ sectionsAttribute }]` );
+          						if ( sectionParts.length !== receivedParts.length ) {
+          							updateFullSection = true;
+          							console.error(`Liquid Ajax Cart: the received HTML for the "${ sectionId }" section has a different quantity of the "${ sectionsAttribute }" containers. The section will be updated completely.`);
+          						} else {
+          							sectionParts.forEach(( sectionPartsItem, sectionPartsItemIndex ) => {
+          								sectionPartsItem.before( receivedParts[sectionPartsItemIndex] );
+										sectionPartsItem.parentElement.removeChild(sectionPartsItem);
+          							});
+          						}
+							}
+						}
+						if ( updateFullSection ) {
+							sectionNode.insertAdjacentHTML('beforeBegin', sections[ sectionId ]);
+							newSectionNode = sectionNode.previousSibling;
+							sectionNode.parentElement.removeChild(sectionNode);
+						}
+
+						// Restore scroll positions
 						for ( let scrollId in scrollAreasList ) {
 							newSectionNode.querySelectorAll(` [${ sectionScrollAreaAttribute }="${ scrollId.replace( noId, '' ) }"] `).forEach(( scrollAreaNode, scrollAreaIndex ) => {
 								if ( scrollAreaIndex + 1 <= scrollAreasList[ scrollId ].length ) {
