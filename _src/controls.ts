ff8db57ac77e2e7c54aafa329e874bb880caa108
Evgeniy Mukhamedjanov <evgeniymuhamedjanov@gmail.com>
2023-06-18 14:23:37 +0500

    Remove toggleClassButton control

--- _src/controls.ts
+++ _src/controls.ts
@@ -1,13 +1,13 @@
 import { cartRequestButtonInit } from './controls/request-button';
 import { cartQuantityInputInit } from './controls/quantity-input';
 import { cartPropertyInputInit } from './controls/property-input';
-import { cartToggleClassButtonInit } from './controls/toggle-class-button';
+// import { cartToggleClassButtonInit } from './controls/toggle-class-button';
 
 function cartControlsInit () {
 	cartRequestButtonInit();
 	cartPropertyInputInit();
 	cartQuantityInputInit();
-	cartToggleClassButtonInit();
+	// cartToggleClassButtonInit();
 }
 
 export { cartControlsInit }
